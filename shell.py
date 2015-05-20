1d5e71885192df463a323a4902a25e866bee8873
Nick Chambers <dtscode@gmail.com>
2015-05-20 14:03:49 -0700

    Modified piping function

--- shell.py
+++ shell.py
@@ -3,17 +3,13 @@
 import os, sys
 
 def execute(args=list):
-  if args[0] == 'cd':
-    os.chdir(args[1])
+  output_file = ''
 
-  else:
-    output_file = ''
-
-    if len(args) > 1 and args[-2] == '>':
-      output_file = args[-1]
-      args = args[:-2]
-      file_descriptor = open(output_file, "w+")
-      os.dup2(file_descriptor.fileno(), sys.stdout.fileno())
+  if len(args) > 1 and args[-2] == '>':
+    output_file = args[-1]
+    args = args[:-2]
+    file_descriptor = open(output_file, "w+")
+    os.dup2(file_descriptor.fileno(), sys.stdout.fileno())
 
   os.execvp(args[0], args)
 
@@ -21,32 +17,36 @@ def pipe_exec(out_command, in_command, readfd, writefd):
   pid = os.fork()
 
   if pid == 0:
-    os.dup2(writefd, sys.stdout.fileno())
-    execute(out_command)
-    os.close(readfd)
-
-  else:
-    os.wait()
     pid2 = os.fork()
 
     if pid2 == 0:
-      os.dup2(readfd, sys.stdin.fileno())
-      execute(in_command)
-      os.close(writefd)
+      os.dup2(writefd, sys.stdout.fileno())
+      execute(out_command)
+      os.close(readfd)
 
     else:
       os.wait()
-  
+      os.dup2(readfd, sys.stdin.fileno())
+      execute(in_command)
+
+  else:
+    os.wait()
 
 def main():
   while True:
     line = input('dtshell$ ')
     args = line.split(' ')
 
-    if args[0] == 'exit':
+    if line == '':
+      continue
+
+    elif args[0] == 'exit':
       sys.exit()
 
-    if '|' in args:
+    elif args[0] == 'cd':
+      os.chdir(args[1])
+
+    elif '|' in args:
       read, write = os.pipe()
       pipe_location = args.index('|')
       pipe_exec(args[:pipe_location], args[pipe_location+1:], read, write)
