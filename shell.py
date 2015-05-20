fd346067d88e97c57d05bb405f4fe3c11ba6f7ab
Nick Chambers <dtscode@gmail.com>
2015-05-20 09:33:56 -0700

    Misc changes

--- shell.py
+++ shell.py
@@ -14,8 +14,8 @@ while True:
     if len(args) > 1 and args[-2] == '>':
       output_file = args[-1]
       args = args[:-2]
-      file_descriptor = open(output_file, "w+")
-      os.dup2(file_descriptor.fileno(), 1)
+      file_descriptor = open(output_file, "w")
+      os.dup2(file_descriptor.fileno(), sys.stdout.fileno())
 
     os.execvp(args[0], args)
 
