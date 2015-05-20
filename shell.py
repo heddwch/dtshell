02aec4356d4a5061c52205016f4b98267f7fe9ad
Nick Chambers <dtscode@gmail.com>
2015-05-20 09:37:07 -0700

    Started support for common builtin commands

--- shell.py
+++ shell.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
-import os
-import string
+import os, sys
 
 while True:
   line = input('dtshell$ ')
   args = line.split(' ')
+
+  if args[0] == 'exit':
+    sys.exit()
+
   pid = os.fork()
 
   if pid == 0:
-    output_file = ''
+    if args[0] == 'cd':
+      os.chdir(args[1])
 
-    if len(args) > 1 and args[-2] == '>':
-      output_file = args[-1]
-      args = args[:-2]
-      file_descriptor = open(output_file, "w+")
-      os.dup2(file_descriptor.fileno(), 1)
+    else:
+      output_file = ''
 
-    os.execvp(args[0], args)
+      if len(args) > 1 and args[-2] == '>':
+        output_file = args[-1]
+        args = args[:-2]
+        file_descriptor = open(output_file, "w+")
+        os.dup2(file_descriptor.fileno(), 1)
+
+      os.execvp(args[0], args)
 
   else:
     os.wait()
