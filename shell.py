0cddb3245591a2c976f06f9cc3b5cabad2c7343a
Nick Chambers <dtscode@gmail.com>
2015-05-20 01:02:32 -0700

    Added argument handling. Started IO redirect (ie >)

--- shell.py
+++ shell.py
@@ -4,11 +4,19 @@ import os
 import string
 
 while True:
-  line = input('> ')
-  args = str.split(line, ' ')
+  line = input('dtshell$ ')
+  args = line.split(' ')
   pid = os.fork()
 
   if pid == 0:
+    redirect = False
+    outputFile = ''
+    for arg in args:
+      if arg == '>':
+        redirect = True
+      if redirect:
+        outputFile = arg
+
     os.execvp(args[0], args)
 
   else:
