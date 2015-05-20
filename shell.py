4ff86c454025a4a0799a4cc82bc1f9adcced29f6
Nick Chambers <dtscode@gmail.com>
2015-05-20 12:01:04 -0700

    Started working on basic piping support

--- shell.py
+++ shell.py
@@ -17,13 +17,26 @@ while True:
 
     else:
       output_file = ''
+      non_piped_command = True
 
       if len(args) > 1 and args[-2] == '>':
         output_file = args[-1]
         args = args[:-2]
         file_descriptor = open(output_file, "w+")
-        os.dup2(file_descriptor.fileno(), 1)
+        os.dup2(file_descriptor.fileno(), sys.stdout.fileno())
 
+      elif len(args) > 1 and '|' in args:
+        pipe_location = args.index('|')
+        out_command = args[:pipe_location]
+        in_command = args[pipe_location+1:]
+        read, write = os.pipe()
+        os.dup2(read, sys.stdout.fileno())
+        os.execvp(out_command[0], out_command)
+        os.dup2(write, sys.stdin.fileno())
+        os.execvp(in_command[0], in_command)
+        non_piped_command = False
+
+    if non_piped_command:
       os.execvp(args[0], args)
 
   else:
